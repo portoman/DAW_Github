b4dc645468229816f963eca5fdd2800127d553b2
portobuj <portbuj@yahoo.es>
2021-09-28 16:27:30 +0200

    Update 20210928

---
+++ 03_Programación/UD01/PROG01_programa1.java
@@ -0,0 +1,12 @@
+public class PROG01_programa1{
+	
+	
+	public static void main(String[]args){
+		System.out.println("Módulo Profesional - PROGRAMACIÓN. UNIDAD DE TRABAJO 01");
+		System.out.println("Introducción a la programación");
+		System.out.println("Jesús Alfonso Porto Bujía");
+		System.out.println("A Coruña, A Coruña");
+		System.out.println("28/09/2021");
+		System.out.println("Programa1");
+		};
+}
---
+++ 03_Programación/UD01/PrimeraAplicNetbeans/src/main/java/daw/primeraaplicnetbeans/PROG01_programa1.java
@@ -0,0 +1,22 @@
+package daw.primeraaplicnetbeans;
+
+public class PROG01_programa1 {
+
+    //Clase principal:
+    public static void main(String[] args) {
+        System.out.println("Módulo Profesional - PROGRAMACIÓN. UNIDAD DE TRABAJO 01");
+	System.out.println("Introducción a la programación");
+        /*Nombre y apellidos
+        del alumno*/
+        System.out.println("Jesús Alfonso Porto Bujía");
+        //Localidad y provincia
+	System.out.println("A Coruña, A Coruña ");
+        //Fecha de realización del ejercicio
+	System.out.println("28/09/2021");
+        /*
+        Número de programa
+        */
+	System.out.println("Programa1");
+    }
+    
+}
